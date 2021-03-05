a310c31a3d0195f863c421b125934d0f5dcc782b
Kaung Shein <nicholasling1412@gmail.com>
2021-03-05 17:34:54 -0500

    IntersectionObserver polished + Framer Motion Setup

--- src/App.js
+++ src/App.js
@@ -3,49 +3,18 @@ import "./App.css";
 
 import Nav from "./components/Nav";
 import SlideOne from "./components/SlideOne";
-// import SlideTwo from "./components/SlideTwo";
-
-function useOnScreen(options) {
-  const [ref, setRef] = React.useState(null);
-  const [visible, setVisible] = React.useState(false);
-
-  React.useEffect(() => {
-    const observer = new IntersectionObserver(([entry]) => {
-      setVisible(entry.isIntersecting);
-    }, options);
-
-    if (ref) {
-      observer.observe(ref);
-    }
-
-    return () => {
-      if (ref) {
-        observer.unobserve(ref);
-      }
-    };
-  }, [ref, options]);
-
-  return [setRef, visible];
-}
+import SlideTwo from "./components/SlideTwo";
 
 function App() {
-  const [setRef, visible] = useOnScreen({ threshold: 0.9 });
-
   return (
     <div className="App">
       <Nav />
-      <SlideOne />
-      <div className="slides">
-        <div
-          className="slide-two-container"
-          ref={setRef}
-          style={{ backgroundColor: visible ? "#23cebd" : "#efefef" }}
-        >
-          <div className="tagline">
-            1.4 million adults in the United States attempted suicide in 2019
-          </div>
-          <div className="tagline-2">that's 4 attempts every 1000 adults</div>
-        </div>
+      <div className="slides-container">
+        <SlideOne />
+        <SlideTwo />
+        <SlideTwo />
+        <SlideTwo />
+        <SlideTwo />
       </div>
     </div>
   );
--- src/components/SlideTwo.js
+++ src/components/SlideTwo.js
@@ -1,18 +1,57 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+function useOnScreen(options) {
+  const [ref, setRef] = React.useState(null);
+  const [visible, setVisible] = React.useState(false);
+
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      if (visible === false) {
+        setVisible(entry.isIntersecting);
+      }
+    }, options);
+
+    if (ref) {
+      observer.observe(ref);
+    }
+
+    return () => {
+      if (ref) {
+        observer.unobserve(ref);
+      }
+    };
+  }, [ref, options, visible]);
+
+  return [setRef, visible];
+}
+
+const variants = {
+  initial: { opacity: 0.2 },
+  animate: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+    },
+  },
+};
 
 const SlideTwo = (props) => {
+  const [setRef, visible] = useOnScreen({ threshold: 0.9 });
   return (
     <div className="slides">
-      <div
+      <motion.div
+        variants={variants}
+        initial="initial"
+        animate={visible ? "animate" : "initial"}
         className="slide-two-container"
-        ref={props.ref}
-        style={{ backgroundColor: props.visible ? "#23cebd" : "#efefef" }}
+        ref={setRef}
       >
         <div className="tagline">
           1.4 million adults in the United States attempted suicide in 2019
         </div>
         <div className="tagline-2">that's 4 attempts every 1000 adults</div>
-      </div>
+      </motion.div>
     </div>
   );
 };
