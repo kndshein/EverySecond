9ffc39ac369d153bc758e9a497fe4fb498fe38f5
Kaung Shein <nicholasling1412@gmail.com>
2021-03-06 20:42:28 -0500

    Set up Slide 1

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,13 +4,13 @@ import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 const Nav = (props) => {
   return (
     <div className="nav-bar-container">
-      <MdKeyboardArrowLeft size={40} />
+      <MdKeyboardArrowLeft size={50} />
       <div className="logo-container">
         <div className="logo">Every Second</div>
         <div className="divider"></div>{" "}
         <div className="logo-tagline">Suicide Prevention</div>
       </div>
-      <MdKeyboardArrowRight size={40} />
+      <MdKeyboardArrowRight size={50} />
     </div>
   );
 };
--- src/components/SlideOne.js
+++ src/components/SlideOne.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { MdPersonPinCircle } from "react-icons/md";
 
 const SlideOne = (props) => {
   return (
     <div className="slides">
       <div className="slide-one-container">
-        <div className="tagline">
-          every 2.6 second, somebody in America is having thoughts of suicide
+        <div className="taglines-container">
+          <div className="tagline">every 2.6 seconds,</div>
+          <div className="tagline-2">
+            somebody in the US is having thoughts of suicide
+          </div>
+        </div>
+        <div className="animation">
+          <MdPersonPinCircle size={50} />
+        </div>
+        <div className="background-image">
+          <img src="https://images.unsplash.com/photo-1615027138404-06f31c35ae1d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" />
         </div>
       </div>
     </div>
