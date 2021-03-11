c93db96bf14aa4f4b8e5aae9b076d312cb8fdc85
Kaung Shein <nicholasling1412@gmail.com>
2021-03-11 15:08:19 -0500

    changed SlideOne loading

--- src/components/SlideOne.js
+++ src/components/SlideOne.js
@@ -2,95 +2,87 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const SlideOne = ({ content }) => {
-  const loaded = () => {
-    var num = [],
-      i = 0,
-      len = 30;
-    while (++i <= len) num.push(i);
+  var num = [],
+    i = 0,
+    len = 30;
+  while (++i <= len) num.push(i);
 
-    const container = {
-      hidden: { opacity: 0 },
-      show: {
-        opacity: 1,
-        transition: {
-          staggerChildren: content?.SlideOne.second,
-          delayChildren: 2 + content?.SlideOne.second / 2,
-        },
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: content?.SlideOne.second,
+        delayChildren: 2 + content?.SlideOne.second / 2,
       },
-    };
+    },
+  };
 
-    const item = {
-      hidden: { opacity: 0 },
-      show: {
-        opacity: 1,
-        transition: { duration: 2.6, ease: "easeOut" },
-      },
-    };
+  const item = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: { duration: 2.6, ease: "easeOut" },
+    },
+  };
 
-    return (
-      <div className="slides">
-        <div className="slide-one-container">
+  return (
+    <div className="slides">
+      <div className="slide-one-container">
+        <motion.div
+          className="taglines-container"
+          animate={{ opacity: [0, 1] }}
+          transition={{ duration: 2 }}
+        >
           <motion.div
-            className="taglines-container"
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 2 }}
+            className="taglines-container-container"
+            initial={{ y: 50 }}
+            animate={{ y: -25 }}
+            transition={{ duration: 1, delay: 1.5 }}
           >
-            <motion.div
-              className="taglines-container-container"
-              initial={{ y: 50 }}
-              animate={{ y: -25 }}
-              transition={{ duration: 1, delay: 1.5 }}
-            >
-              <div className="tagline">
-                every{" "}
-                <motion.span
-                  animate={{ opacity: [1, 0] }}
-                  transition={{
-                    duration: content?.SlideOne.second / 2,
-                    yoyo: Infinity,
-                    delay: 2,
-                  }}
-                  ease={{ ease: "easeInOut" }}
-                >
-                  {content?.SlideOne.second}
-                </motion.span>{" "}
-                {content?.SlideOne.second > 1 ? "seconds," : "second,"}
-              </div>
-              <div className="tagline-2">{content?.SlideOne.tagline}</div>
-            </motion.div>
-            <motion.div
-              className="icons-container"
-              variants={container}
-              initial="hidden"
-              animate="show"
-            >
-              {num.map((x, index) => (
-                <motion.div variants={item} key={index}>
-                  <img
-                    className="icon"
-                    src={`${content?.SlideOne.icon.img}`}
-                    alt={`${content?.SlideOne.icon.alt}`}
-                  />
-                </motion.div>
-              ))}
-            </motion.div>
+            <div className="tagline">
+              every{" "}
+              <motion.span
+                animate={{ opacity: [1, 0] }}
+                transition={{
+                  duration: content?.SlideOne.second / 2,
+                  yoyo: Infinity,
+                  delay: 2,
+                }}
+                ease={{ ease: "easeInOut" }}
+              >
+                {content?.SlideOne.second}
+              </motion.span>{" "}
+              {content?.SlideOne.second > 1 ? "seconds," : "second,"}
+            </div>
+            <div className="tagline-2">{content?.SlideOne.tagline}</div>
           </motion.div>
-          <div className="background-image">
-            <img
-              src={`${content?.SlideOne.background.img}`}
-              alt={`${content?.SlideOne.background.alt}`}
-            />
-          </div>
+          <motion.div
+            className="icons-container"
+            variants={container}
+            initial="hidden"
+            animate="show"
+          >
+            {num.map((x, index) => (
+              <motion.div variants={item} key={index}>
+                <img
+                  className="icon"
+                  src={`${content?.SlideOne.icon.img}`}
+                  alt={`${content?.SlideOne.icon.alt}`}
+                />
+              </motion.div>
+            ))}
+          </motion.div>
+        </motion.div>
+        <div className="background-image">
+          <img
+            src={`${content?.SlideOne.background.img}`}
+            alt={`${content?.SlideOne.background.alt}`}
+          />
         </div>
       </div>
-    );
-  };
-
-  const loading = () => {
-    return "Loading";
-  };
-
-  return content?.SlideOne ? loaded() : loading();
+    </div>
+  );
 };
 
 export default SlideOne;
